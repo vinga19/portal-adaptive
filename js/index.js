ac94f0671383ab965624e9f736e3b80d01ee12de
vinga19 <vitorhugo_rib@hotmail.com>
2019-12-02 12:14:55 -0300

    first commit

---
+++ js/index.js
@@ -0,0 +1,14 @@
+
+function bigImg(x){
+    
+    x.style.height = "100px"
+    x.style.width = "100px"
+    
+    
+}
+
+function normalImg(x) {
+    x.style.height = "65px";
+    x.style.width = "65px"
+    
+  }
